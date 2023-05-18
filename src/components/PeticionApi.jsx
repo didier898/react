db91f0e6aecbdcc90b22a861e2dd0c30d8ce556a
Didier <didier002010@gmail.com>
2023-05-18 17:05:04 -0500

    peticion al api de morty

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
 
 const PeticionApi = () => {
+    const [personajes,setPersonajes]=React.useState([])
+    const[paginacion,setPaginacion]= React.useState(0)
+    const TraerPersonajes = async()=>{
+        try {
+            const res= await fetch(` https://rickandmortyapi.com/api/character/?page=${paginacion}` )
+            const{results}= await res.json()
+            setPersonajes(results) 
+            console.log(results)
+        } catch (error) { 
+            console.log(error)
+            
+        }
+        
+
+    }
+
+    const siguiente = ()=>{ 
+        setPaginacion(paginacion+1)
+        TraerPersonajes()
+
+    }
+
+    const atras =()=>{
+        setPaginacion(paginacion-1)
+        TraerPersonajes()
+
+    }
+   
   return (
-    <div><h1>Peticion al api de rick and morty</h1></div>
+    <div>
+        <h1>Peticion al api de rick and morty</h1>
+        <button onClick={TraerPersonajes}>TraerPersonajes</button>
+        <button onClick={atras}>atras</button>
+        <button onClick={siguiente}>siguiente</button>
+        {
+            personajes.map(({id,name,image})=> (
+                <div key={id}>
+                    <h4>{id}-{name}</h4>
+                    <img src={image} alt={name} />
+                </div>
+            )
+            )
+        }
+        
+
+       
+    </div>
+      
+
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 //import Variables from './components/Variables';
-import Contador from './components/Contador';
+//import Contador from './components/Contador';
+import PeticionApi from './components/PeticionApi';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Contador />
+    <PeticionApi />
   </React.StrictMode>
 );
 
